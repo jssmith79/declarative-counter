d3004b894eb22af191512191a5bbc293654efecb
jssmith79 <i.am.hef@gmail.com>
2023-04-04 21:01:50 -0700

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React,  {useState} from 'react';
 
 function App() {
+  let [ counterNum, setCounterNum ] = useState(0)
+  window.setCounterNum = setCounterNum
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+ 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         { counterNum }
         </p>
         <a
           className="App-link"
